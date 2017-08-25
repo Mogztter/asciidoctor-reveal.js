f77165b2a523b0ebbfa7547bc43716f4aec94ccf
Olivier Bilodeau <olivier@bottomlesspit.org>
2017-08-24 23:17:52 -0400

    Added html_tag helper from asciidoctor-html5s project
    from https://github.com/jirutka/asciidoctor-html5s/blob/a71db48a1dd5196b668b3a3d93693c5d877c5bf3/data/templates/helpers.rb#L84-L108

--- templates/slim/helpers.rb
+++ templates/slim/helpers.rb
@@ -31,6 +31,38 @@ module Slim::Helpers
     val && val != 'false' && val.to_s != '0' || false
   end
 
+  ##
+  # This function is from the asciidictor-html5s project
+  # Copyright 2014-2017 Jakub Jirutka <jakub@jirutka.cz> and the Asciidoctor Project.
+  # Licensed under the MIT License.
+  # https://github.com/jirutka/asciidoctor-html5s/blob/a71db48a1dd5196b668b3a3d93693c5d877c5bf3/data/templates/helpers.rb#L84-L108
+  #
+  # Creates an HTML tag with the given name and optionally attributes. Can take
+  # a block that will run between the opening and closing tags.
+  #
+  # @param name [#to_s] the name of the tag.
+  # @param attributes [Hash] (default: {})
+  # @param content [#to_s] the content; +nil+ to call the block. (default: nil).
+  # @yield The block of Slim/HTML code within the tag (optional).
+  # @return [String] a rendered HTML element.
+  #
+  def html_tag(name, attributes = {}, content = nil)
+    attrs = attributes.inject([]) do |attrs, (k, v)|
+      next attrs if !v || v.nil_or_empty?
+      v = v.compact.join(' ') if v.is_a? Array
+      attrs << (v == true ? k : %(#{k}="#{v}"))
+    end
+    attrs_str = attrs.empty? ? '' : attrs.join(' ').prepend(' ')
+
+
+    if VOID_ELEMENTS.include? name.to_s
+      %(<#{name}#{attrs_str}>)
+    else
+      content ||= yield if block_given?
+      %(<#{name}#{attrs_str}>#{content}</#{name}>)
+    end
+  end
+
 end
 
 # More custom functions can be added in another namespace if required

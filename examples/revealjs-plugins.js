3a9cfeb8e6be5fbe88f1b56b1028c6e3dacb5e09
Guillaume Grossetie <ggrossetie@gmail.com>
2021-02-21 18:20:45 +0100

    Upgrade to reveal.js 4.1.0

--- examples/revealjs-plugins.js
+++ examples/revealjs-plugins.js
@@ -1,2 +1,2 @@
-{ src: 'revealjs-plugins/reveal.js-menu/menu.js' },
-{ src: 'revealjs-plugins/chalkboard/chalkboard.js' }
+{ src: 'revealjs-plugins/menu/plugin.js' },
+{ src: 'revealjs-plugins/chalkboard/plugin.js' }
